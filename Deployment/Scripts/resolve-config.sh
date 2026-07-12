3a3e630f96301e9c2c7b8be2c1afcccf49f082f5
Wiktor Wijata <wiktorwijata@gmail.com>
2026-07-12 22:43:35 +0200

    Add CI/CD, Docker, and production config for deployment

---
+++ Deployment/Scripts/resolve-config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+mkdir -p config
+
+# DbMigrator
+sed \
+  -e "s/{db_host}/${DB_HOST}/g" \
+  -e "s/{db_port}/${DB_PORT}/g" \
+  -e "s/{db_password}/${DB_PASSWORD}/g" \
+  -e "s/{redis_host}/${REDIS_HOST}/g" \
+  -e "s/{redis_port}/${REDIS_PORT}/g" \
+  -e "s/{redis_password}/${REDIS_PASSWORD}/g" \
+  templates/migrator/appsettings.Production.json > config/migrator.appsettings.Production.json
+
+# API
+sed \
+  -e "s/{db_host}/${DB_HOST}/g" \
+  -e "s/{db_port}/${DB_PORT}/g" \
+  -e "s/{db_password}/${DB_PASSWORD}/g" \
+  -e "s/{redis_host}/${REDIS_HOST}/g" \
+  -e "s/{redis_port}/${REDIS_PORT}/g" \
+  -e "s/{redis_password}/${REDIS_PASSWORD}/g" \
+  templates/api/appsettings.Production.json > config/api.appsettings.Production.json
