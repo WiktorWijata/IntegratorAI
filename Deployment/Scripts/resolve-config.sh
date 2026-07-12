e535412d1b72a53559d11798a4bf7b00dfe3e2a1
Wiktor Wijata <wiktorwijata@gmail.com>
2026-07-12 23:30:34 +0200

    fix

--- Deployment/Scripts/resolve-config.sh
+++ Deployment/Scripts/resolve-config.sh
@@ -4,18 +4,13 @@ set -e
 mkdir -p config
 
 # DbMigrator
-sed \
-  -e "s|{db_host}|${DB_HOST}|g" \
-  -e "s|{db_port}|${DB_PORT}|g" \
-  -e "s|{db_password}|${DB_PASSWORD}|g" \
-  templates/migrator/appsettings.Production.json > config/migrator.appsettings.Production.json
+export DB_HOST DB_PORT DB_PASSWORD
+envsubst '${DB_HOST}${DB_PORT}${DB_PASSWORD}' \
+  < templates/migrator/appsettings.Production.json \
+  > config/migrator.appsettings.Production.json
 
 # API
-sed \
-  -e "s|{db_host}|${DB_HOST}|g" \
-  -e "s|{db_port}|${DB_PORT}|g" \
-  -e "s|{db_password}|${DB_PASSWORD}|g" \
-  -e "s|{redis_host}|${REDIS_HOST}|g" \
-  -e "s|{redis_port}|${REDIS_PORT}|g" \
-  -e "s|{redis_password}|${REDIS_PASSWORD}|g" \
-  templates/api/appsettings.Production.json > config/api.appsettings.Production.json
+export DB_HOST DB_PORT DB_PASSWORD REDIS_HOST REDIS_PORT REDIS_PASSWORD
+envsubst '${DB_HOST}${DB_PORT}${DB_PASSWORD}${REDIS_HOST}${REDIS_PORT}${REDIS_PASSWORD}' \
+  < templates/api/appsettings.Production.json \
+  > config/api.appsettings.Production.json
